d1ec99c6493d0c359b4306c1be72f9679515fe4e
Nikita <57098717+97nik@users.noreply.github.com>
2020-06-27 15:10:36 +0300

    сделал настройку цвета, но не понял зачем делить значение цвета на 2 - далее надо добавить label для слайдоров

--- slider/ViewController.swift
+++ slider/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+  
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var sliderOne: UISlider!
+    @IBOutlet weak var sliderTwo: UISlider!
+    
+    @IBOutlet weak var sliderThree: UISlider!
 
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+           
+        // Slider
+        sliderOne.value = 1
+        sliderOne.minimumValue = 0
+        sliderOne.maximumValue = 250
+        
+        
+        sliderTwo.value = 1
+        sliderTwo.minimumValue = 0
+        sliderTwo.maximumValue = 250
+        
+        sliderThree.value = 1
+        sliderThree.minimumValue = 0
+        sliderThree.maximumValue = 250
+        
+    
+       colorView.backgroundColor = UIColor.init(red: 200, green:100 , blue: 100, alpha: 1)
+        
     }
 
-
+    
+    
+    @IBAction func sliderA() {
+        let sliderValue = CGFloat(sliderOne.value)/250
+         let sliderValueTwo = CGFloat(sliderTwo.value)
+        
+        colorView.backgroundColor = UIColor.init(red: sliderValue, green:sliderValueTwo , blue: 0, alpha: 1)
+    }
+    
+    @IBAction func sliderAB() {
+        let sliderValue = CGFloat(sliderOne.value)
+         let sliderValueTwo = CGFloat(sliderTwo.value)/250
+         colorView.backgroundColor = UIColor.init(red: sliderValue, green:sliderValueTwo , blue: 0, alpha: 1)
+        
+    }
+    
+    @IBAction func sliderAC() {
+        let sliderValue = CGFloat(sliderOne.value)
+        let sliderValueTwo = CGFloat(sliderTwo.value)/250
+        let sliderValueThree = CGFloat(sliderThree.value)/250
+                colorView.backgroundColor = UIColor.init(red: sliderValue, green:sliderValueTwo , blue: sliderValueThree, alpha: 1)
+    }
+    
 }
-
